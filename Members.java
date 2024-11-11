92c66e17d3e1fa40ccdc160da3e42614e86fedd6
jayanthnaidu1 <jayanthnaiduappikonda@gmail.com>
2024-11-11 19:01:50 +0530

    Members.java

---
+++ Members.java
@@ -0,0 +1,35 @@
+package com.librarymanagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member
+{
+    private String memberId;
+    private String name;
+    private String email;
+    private List<Book> borrowedBooks;
+
+    public Member(String memberId, String name, String email)
+    {
+        this.memberId = memberId;
+        this.name = name;
+        this.email = email;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    // Getter Methods
+    public String getMemberId()
+    { return memberId;
+    }
+    public String getName() 
+    { return name; 
+    }
+    public String getEmail() 
+    { return email;
+    }
+    public List<Book> getBorrowedBooks() 
+    { 
+    	
+    	return borrowedBooks;
+    }
