efcff4298ca04d3ebec9d6b840da63d8268c2e20
jayanthnaidu1 <jayanthnaiduappikonda@gmail.com>
2024-11-12 21:19:31 +0530

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,1 +1,14 @@
+package com.librarymanagement;
+import java.io.*;
+import java.util.*;
+public class Library 
+{
+    private Map<String, Book> books;
+    private Map<String, Member> members;
 
+    public Library() 
+    {
+        books = new HashMap<>();
+        members = new HashMap<>();
+    }
+}
