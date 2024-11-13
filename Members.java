8d31bd23fef192d892a4ae6d02089cb3f22ef11c
jayanthnaidu1 <jayanthnaiduappikonda@gmail.com>
2024-11-13 22:15:42 +0530

    Update Members.java

--- Members.java
+++ Members.java
@@ -1,35 +1,48 @@
-package com.librarymanagement;
-
+package com.librarymangement;     //userdefined package
+// importing packages
 import java.util.ArrayList;
 import java.util.List;
 
-public class Member
+public class Member   // constructor
 {
     private String memberId;
     private String name;
     private String email;
     private List<Book> borrowedBooks;
 
-    public Member(String memberId, String name, String email) // constructor
+    public Member(String memberId, String name, String email)
     {
         this.memberId = memberId;
         this.name = name;
         this.email = email;
         this.borrowedBooks = new ArrayList<>();
     }
-
     // Getter Methods
     public String getMemberId()
-    { return memberId;
+    {
+      return memberId;
     }
     public String getName() 
-    { return name; 
+    { 
+      return name; 
     }
     public String getEmail() 
-    { return email;
+    { 
+      return email;
     }
     public List<Book> getBorrowedBooks() 
     { 
     	
     	return borrowedBooks;
     }
+    // For borrowing books
+    public void borrowBook(Book book) 
+    {
+        borrowedBooks.add(book);
+    }
+    //For Returning books
+    public void returnBook(Book book)
+    {
+        borrowedBooks.remove(book);
+    }
+}
