8d231233afd7bcf3cddfe8370de3926318d34676
jayanthnaidu1 <jayanthnaiduappikonda@gmail.com>
2024-11-13 19:16:37 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,35 +1,46 @@
-package com.librarymanagement;
-
+//user defined package
+package com.librarymangement;
 public class Book 
 {
-    private String bookId;
-    private String title;
-    private String author;
-    private String genre;
-    private boolean isAvailable;
-
-    // Constructor
-    public Book(String bookId, String title, String author, String genre)
-    {
-        this.bookId = bookId;
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-    }
-    public String getBookId()
-   {
-     return bookId;
-   }
-    public String getTitle() 
-   { 
-     return title; 
-   }
-    public String getAuthor()
-   { 
-     return author;
-   }
-    public String getGenre()
-   {
-    return genre; 
-   }      
+	    private String bookId;
+	    private String title;
+	    private String author;
+	    private String genre;
+	    private boolean isAvailable;
+    
+	    // Constructor
+	    public Book(String bookId, String title, String author, String genre) 
+	    {
+	        this.bookId = bookId;
+	        this.title = title;
+	        this.author = author;
+	        this.genre = genre;
+	        this.isAvailable = true; //Setting Initially the book is available
+	    }
+	    // Getters Methods
+	    public String getBookId()
+	    { 
+            return bookId;
+	    }
+	    public String getTitle() 
+	    {
+            return title; 
+	    }
+	    public String getAuthor() 
+	    { 
+            return author; 
+	    }
+	    public String getGenre() 
+	    { 
+            return genre; 
+	    }
+	    public boolean isAvailable() 
+	    {
+            return isAvailable; 
+	    }
+	    public void setAvailable(boolean available)
+	    { 
+            isAvailable = available;
+	    }
 }
+
